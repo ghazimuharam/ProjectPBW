64962e4ad0cfde190e210323a2dd2ba385c03a19
unknown <farhankaromy@gmail.com>
2020-04-10 14:55:19 +0700

    tambah halaman voting

---
+++ resources/views/vote/voting.blade.php
@@ -0,0 +1,103 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>Portal Pemilihan</title>
+  </head>
+  <body>
+    <section id="Navbar">
+      <div class="container-fluid p-0">
+        <nav class="navbar navbar-light bg-darkblue justify-content-around">
+          <a class="navbar-brand"  href="index.html"> <h3>Vilection</h3></a>
+          <h4>Welcome</h4>
+          <a class="text-secondary" href="#"><h4>Logout</h4></a>
+        </nav>
+      </div>
+   </section>
+
+   <section id="alert">
+     <div class="container p-4">
+      <div class="alert alert-secondary text-center" role="alert">
+        Selamat datang di halaman pemilihan, gunakan hak pilih Anda dengan bijak.
+      </div>
+     </div>
+   </section>
+
+   <section id="kandidat">
+     <div class="container text-center">
+      <div class="row ">
+        <div class="col-md-3">
+          <div class="card">
+            <img class="card-img-top" src="pp.png" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-primary">Vote me!</a>
+            </div>
+          </div>
+        </div>
+
+        <div class="col-md-3">
+          <div class="card">
+            <img class="card-img-top" src="pp.png" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-primary">Vote me!</a>
+            </div>
+          </div>
+        </div>
+        
+        <div class="col-md-3">
+          <div class="card">
+            <img class="card-img-top" src="pp.png" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-primary">Vote me!</a>
+            </div>
+          </div>
+        </div>
+        
+        <div class="col-md-3">
+          <div class="card">
+            <img class="card-img-top" src="pp.png" alt="Card image cap">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+              <a href="#" class="btn btn-primary">Vote me!</a>
+            </div>
+          </div>
+        </div>
+
+
+
+
+        
+      </div>
+    </div>
+   </section>
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+   
+  </body>
+</html>
+
+<style>
+
+  .bg-darkblue{
+    background-color: #061C30;
+  }
+  
+  h3,h4{
+    color: #CCCCCC;
+  }
+</style>
