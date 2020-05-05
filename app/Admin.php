75ae464f96f0c5e4f4aef1d8a1d4e02c21dc7abb
Teuku Zafran Elrifqi <t.zafran@mhs.unsyiah.ac.id>
2020-05-05 15:46:17 +0700

    add profile

--- app/Admin.php
+++ app/Admin.php
@@ -6,5 +6,5 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 class Admin extends Authenticatable
 {
-    //
+    protected $fillable = ['email', 'username', 'password'];
 }
--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Http\Request;
 use App\Admin;
 use App\Website;
@@ -77,9 +78,25 @@ class AdminController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function edit($id)
+    public function edit(Request $request)
     {
-        //
+        $request->validate([
+            'email' => 'required',
+            'username' => 'required',
+            'password' => 'required',
+        ]);
+        
+        $update = Admin::first()->update([
+            'email' => $request->email,
+            'username' => $request->username,
+            'password' => Hash::make($request->password),
+        ]);
+
+        if ($update) {
+            return redirect('admin/profile')->with('notif', 'Data admin updated successfully!');
+        } else {
+            return redirect('admin/profile')->with('error', 'Error!');
+        }
     }
 
     /**
@@ -122,5 +139,12 @@ class AdminController extends Controller
         ]);
 
         return redirect('/admin/websiteManagement')->with('success', 'Edited Successfully');
+    }
+
+    public function profile()
+    {
+        $data = Admin::first();
+
+        return view('admin.profile', ['admin' => $data]);
     }
 }
